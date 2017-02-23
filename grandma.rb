0ffd6de4ac2fbb0ee45119c02a0bb5212aa3c6b3
Ethan Ryan <ethanryan@gmail.com>
2017-02-23 20:25:25 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,30 @@
 # Write a speak_to_grandma method.
 
+def speak_to_grandma(input) #input is the argument, whatever grandchild is saying
+  if (input != input.upcase) #if input isn't in all caps... (why can't i say 'if (input == input.downcase)' here instead??? )
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif ((input == input.upcase) && (input != "I LOVE YOU GRANDMA!")) #if input is in all caps, and isn't "I LOVE YOU GRANDMA!"...
+    return "NO, NOT SINCE 1938!"
+  elsif ((input == input.upcase) && (input == "I LOVE YOU GRANDMA!")) #if input is in all caps, and is "I LOVE YOU GRANDMA!"...
+    return "I LOVE YOU TOO PUMPKIN!"
+  end #end if...else statement
+end #end method
+
+# def speak_to_grandma(argument)
+#   if argument == "I LOVE YOU GRANDMA!"
+#     return "I LOVE YOU TOO PUMPKIN!"
+#   elsif argument.upcase
+#     return "NO, NOT SINCE 1938!"
+#   elsif argument.downcase
+#     return "HUH?! SPEAK UP, SONNY!"
+#   end #end if...else statement
+# end #end method
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
